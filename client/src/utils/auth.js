4575b8c1984edf489451fbb71acf750be9a612f4
newprice247 <newprice247@gmail.com>
2024-03-03 09:52:31 -0500

    add server and client jwt auth functionality

---
+++ client/src/utils/auth.js
@@ -0,0 +1,79 @@
+import decode from 'jwt-decode';
+
+class AuthService {
+
+    getProfile() {
+        if (this) {
+            console.log(`${this} token retrieved`);
+            return decode(this.getToken);
+        } else {
+            console.log('No token available for AuthService')
+        }
+    }
+
+    loggedIn() {
+        if(this) {
+            const token = this.getToken();
+            console.log('Token retrieved for Login');
+            return !!token && !this.isTokenExpired(token);
+        }
+    }
+
+    isTokenExpired(token) {
+        try {
+            const decoded = decode(token);
+            if (decoded.exp < Date.now() / 1000) {
+                return true;
+            } else return false;
+        } catch (err) {
+            console.log('Error retrieving token for token expiration check');
+            console.log(err);
+            return false;
+        }
+    }
+
+    getToken() {
+        console.log('Retrieving Token');
+        try {
+            return localStorage.getItem('id_token');
+        } catch (error) {
+            console.log('Error retrieving token for token get');
+            console.log(error);
+        }
+    }
+
+    login(idToken) {
+        try {
+            localStorage.setItem('id_token', idToken);
+            window.location.assign('/');
+            console.log('Login Successful with ID token')
+        } catch (error) {
+            console.log('Error Logging in with ID token');
+            console.log(error);
+        }
+    }
+
+    logout() {
+        try {
+            localStorage.removeItem('id_token');
+            window.location.assign('/');
+            console.log('Logout Successful');
+        } catch (error) {
+            console.log('Error removing id_token from local storage for logout');
+            console.log(error)
+        }
+    }
+
+    register(idToken) {
+        try {
+            localStorage.setItem('id_token', idToken);
+            window.location.assign('/');
+            console.log('Registration Successful');
+        } catch (error) {
+            console.log('Error in Registration Attempt');
+            console.log(error)
+        }
+    }
+}
+
+export default new AuthService;
--- server/controllers/user/userControllers.js
+++ server/controllers/user/userControllers.js
@@ -1,14 +1,14 @@
 // Here is where we define the user controller
 require("dotenv").config();
-const axios = require("axios");
-const mongodb = require("mongodb");
-const connection = require("../../config/connection");
 const { User } = require("../../models");
+
+const { signToken } = require('../../utils/auth')
 
 
 module.exports = {
     async getUsers(req, res) {
         await User.find({})
+            .select('-__v -password')
             .then(users => {
                 res.json(users);
             })
@@ -16,31 +16,64 @@ module.exports = {
                 res.status(422).json(err);
             });
     },
-    async getUser(req, res) {
-        await User.findById(req.params.id)
+    async getUser({ user = null, params }, res) {
+        await User.findOne({
+            $or: [{ _id: user ? user._id : params.id }, { username: params.username }]
+        })
+            .select('-__v -password')
+            .populate('characters')
             .then(user => {
                 res.json(user);
             })
             .catch(err => {
                 res.status(422).json(err);
             });
-    }, 
+    },
     async createUser(req, res) {
-        await User.create(req.body)
-            .then(newUser => {
-                res.json(newUser);
-            })
-            .catch(err => {
-                res.status(422).json(err);
-            });
+        try {
+            const user = await User.create(req.body);
+            const token = signToken()
+            res.json({ token, user })
+        } catch (error) {
+            console.log('Could not create user with this request');
+            console.log(error);
+        }
+
     },
-    async updateUser(req, res) {
-        await User.findOneAndUpdate({ _id: req.params.id }, req.body)
-            .then(user => {
-                res.json(user);
-            })
-            .catch(err => {
-                res.status(422).json(err);
-            });
+    async updateUser({ params, body }, res) {
+        try {
+            const updatedUser = await User.findByIdAndUpdate(
+                { _id: params.id },
+                { $set: body },
+                { new: true, runValidators: true }
+            );
+            if (!updatedUser) {
+                return res.status(400).json({ message: "Could not find a user with this id" });
+            }
+        } catch (error) {
+            console.log('Update User Unsuccessful');
+            console.log(error);
+            res.status(400).json({ message: "Not able to update user with these parameters"});
+        }
     },
+    async login({body}, res) {
+        try {
+            const user = await User.findOne({
+                $or: [{username: body.username}, {email: body.email}]
+            });
+            if (!user) {
+                console.log(`Login Failed for ${user.username || user.email}`);
+                return res.status(400).json({ message: "Could not find a user with this username or email"});
+            }
+            const correctPw = await user.isCorrectPassword(body.password);
+            if (!correctPw) {
+                return res.status(400).json({ message: "Wrong Password!" });
+            }
+            const token = signToken(user);
+            res.json({ token, user });
+        } catch (error) {
+            res.status(400).json({ message: "Sorry, something went wrong!" });
+            console.log('Login attempt failed');
+        }
+    }
 };
