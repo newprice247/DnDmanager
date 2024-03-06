093779418d3501ea6e7b87d436e2981dcb189397
newprice247 <newprice247@gmail.com>
2024-03-06 16:33:47 -0500

    add character card to user profile test page

--- client/src/pages/UserProfile.jsx
+++ client/src/pages/UserProfile.jsx
@@ -2,6 +2,33 @@ import React, { useState, useEffect } from "react";
 import search from "../utils/API";
 import { Link } from "react-router-dom";
 import Auth from "../utils/auth";
+import {
+    Card
+} from "@material-tailwind/react";
+
+function CharacterCard(props) {
+    return (
+        <Card>
+            <Card.Body>
+                <Link to={`/characters/${props._id}`}>
+                    <h3>{props.name}</h3>
+                </Link>
+                <div
+                className="flex flex-col items-center justify-center w-full h-full"
+                >
+                    <p>Level: {props.level}</p>
+                    <p>Class: {props.class}</p>
+                    <p>Race: {props.race}</p>
+                    <p>Background: {props.background}</p>
+                    <p>Alignment: {props.alignment}</p>
+                    <p>Experience: {props.experience}</p>
+                </div>
+
+
+            </Card.Body>
+        </Card>
+    );
+}
 
 export default function UserProfile() {
     const [user, setUser] = useState({
@@ -24,11 +51,6 @@ export default function UserProfile() {
             .catch((error) => console.error("Error fetching data:", error));
     }, [userId]);
 
-    useEffect(() => {
-        console.log(user);
-    }
-        , [user]);
-
     return (
         <div
             className="flex flex-col items-center justify-center w-full h-full"
@@ -50,17 +72,17 @@ export default function UserProfile() {
                     </h2>
                     <div className="flex flex-col items-center justify-center w-full h-full">
                         {user.characters.map((character) => (
-                            <div
+                            <CharacterCard
                                 key={character._id}
-                                className="flex flex-col items-center justify-center w-full h-full"
-                            >
-                                <h3 className="text-xl font-semibold text-blue-gray-700">
-                                    {character.name}
-                                </h3>
-                                <p className="text-blue-gray-500">
-                                    {character.class} - Level {character.level}
-                                </p>
-                            </div>
+                                _id={character._id}
+                                name={character.name}
+                                level={character.level}
+                                class={character.class}
+                                race={character.race}
+                                background={character.background}
+                                alignment={character.alignment}
+                                experience={character.experience}
+                            />
                         ))}
                     </div>
                 </div>
